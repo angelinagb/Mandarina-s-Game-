cd3a5e646d721bacd9e51494a719ebc2f901fa69
Tomás Civiero <tomasfciviero@gmail.com>
2025-05-21 22:18:54 -0300

    Mejoras rooms

--- new-game-project/rooms/room.gd
+++ new-game-project/rooms/room.gd
@@ -7,28 +7,6 @@ func _ready(): pass
 
 func get_room_identifier(): return room_identifier
 
-func init_items(item_dict: Array[String]):
-	for child: Item in $Interactables/Items.get_children():
-		if item_dict.has(child.getId()):
-			child.start()
-			child.interacted.connect(_on_interactable_interacted)
-
-func init_transitioners(transitioner_dict: Array[String]):
-	for child: Transitioner in $Interactables/Transitioners.get_children():
-		child.start()
-		child.interacted.connect(_on_interactable_interacted)
-
-func init_npc(npc_dict: Array[String]):
-	for child: Npc in $Interactables/Npcs.get_children():
-		child.start()
-		child.interacted.connect(_on_interactable_interacted)
-
-func start_interactables():
-	for child in $Interactables.get_children():
-		if child is Interactable:
-			child.start()
-			child.interacted.connect(_on_interactable_interacted)
-
 func get_position_spawn(name_spawn: String) -> Vector2:
 	for child in $Spawns.get_children():
 		if child is Marker2D and child.name == name_spawn:
