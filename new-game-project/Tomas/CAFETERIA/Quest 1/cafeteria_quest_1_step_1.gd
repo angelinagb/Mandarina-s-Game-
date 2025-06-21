0118c91dace54b54f6a6cfac84b172e76fe3e4aa
Tomás Civiero <tomasfciviero@gmail.com>
2025-06-20 21:34:34 -0300

    Arreglo cafeteria

--- new-game-project/Tomas/CAFETERIA/Quest 1/cafeteria_quest_1_step_1.gd
+++ new-game-project/Tomas/CAFETERIA/Quest 1/cafeteria_quest_1_step_1.gd
@@ -2,8 +2,12 @@ extends QuestStep
 
 @export var minijuego : PackedScene
 @export var dialogo_final : PackedScene
+@export var quest_completada : PackedScene
 
 func on_completed():
 	QueueManager.enqueue_event(minijuego)
 	await QueueManager.finished
 	QueueManager.enqueue_event(dialogo_final)
+	await QueueManager.finished
+	QueueManager.enqueue_event(quest_completada)
+	await QueueManager.finished
--- new-game-project/Tomas/CAFETERIA/nerd_script.gd
+++ new-game-project/Tomas/CAFETERIA/nerd_script.gd
@@ -2,12 +2,15 @@ extends Npc
 
 @export var puzzle : PackedScene
 @export var dialogo_victoria : PackedScene
+@export var quest_completada : PackedScene
 
 func interact():
-	super.interact()
+	await super.interact()
 	QueueManager.enqueue_event(puzzle)
 	await QueueManager.finished
 	QueueManager.enqueue_event(dialogo_victoria)
 	await QueueManager.finished
-	
+	QueueManager.enqueue_event(quest_completada)
+	await QueueManager.finished
+	process_mode = Node.PROCESS_MODE_DISABLED
 	
